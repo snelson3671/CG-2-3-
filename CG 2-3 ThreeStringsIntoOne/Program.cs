efb23dba18af9729669b15c65ceed83a05916400
Sandra <snelson3671@yahoo.com>
2018-07-26 08:16:16 -0500

    turning in CG 2-3

--- CG 2-3 ThreeStringsIntoOne/Program.cs
+++ CG 2-3 ThreeStringsIntoOne/Program.cs
@@ -6,7 +6,18 @@ namespace CG_2_3_ThreeStringsIntoOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's combine Three strings into one");
+            Console.Write("Please type your full name ");
+            String myName = Console.ReadLine();
+
+            Console.Write("What's your favorite color? ");
+            String color = Console.ReadLine();
+
+            Console.Write("Who is your best friend? ");
+            String bestFriend = Console.ReadLine();
+
+            Console.WriteLine("Hi! My name is " + myName + ".  " + "My favorite color is " + color + ".  " + "My best friend is " + bestFriend);
+            Console.ReadLine();
         }
     }
 }
